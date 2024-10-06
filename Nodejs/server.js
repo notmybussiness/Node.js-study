1ebd97365b97dbcab14a68333cad6ee9a36bf6f5
notmybusiness <cuisine97@naver.com>
2024-10-06 16:00:17 +0900

    mongoDB까지 연결

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const app = express()
 
-app.listen(8080, ()=>{
+app.use(express.static(__dirname + '/public'))
+
+const { MongoClient } = require('mongodb')
+
+let db
+const url = 'mongodb+srv://admin:qwe123@forum.8qdxb.mongodb.net/?retryWrites=true&w=majority&appName=forum'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('forum')
+  app.listen(8080, ()=>{
     console.log('http://localhost:8080 에서 서버 실행중')
 })
+}).catch((err)=>{
+  console.log(err)
+})
+
+
 
 app.get('/', (요청, 응답) => {
-    응답.send('반갑다')
+    응답.sendFile(__dirname + '/index.html')
 })
 
 app.get('/news', (요청, 응답) => {
     응답.send('Today News~')
+    db.collection('post').insertOne({title: '어쩌구 쌸라샬라 테스트용'})
 })
