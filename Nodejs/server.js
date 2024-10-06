206e275db802b83d905d7b0ed7ea55ac514a0c4b
notmybusiness <cuisine97@naver.com>
2024-10-06 15:14:24 +0900

    새로운 페이지로 연결  만들기

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -5,6 +5,10 @@ app.listen(8080, ()=>{
     console.log('http://localhost:8080 에서 서버 실행중')
 })
 
-app.get('/', (응답, 요청) => {
-    응답.send('반갑다');
+app.get('/', (요청, 응답) => {
+    응답.send('반갑다')
+})
+
+app.get('/news', (요청, 응답) => {
+    응답.send('Today News~')
 })
