eaa902d3828d1beb8172b08ccfd158b31d390071
notmybusiness <cuisine97@naver.com>
2024-10-08 01:19:04 +0900

    form->server->DB

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -3,7 +3,8 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
-
+app.use(express.json())
+app.use(express.urlencoded({extended:true})) 
 
 
 const { MongoClient } = require('mongodb')
@@ -20,8 +21,6 @@ new MongoClient(url).connect().then((client)=>{
   console.log(err)
 })
 
-
-
 app.get('/', (요청, 응답) => {
     응답.sendFile(__dirname + '/index.html')
 })
@@ -32,12 +31,19 @@ app.get('/', (요청, 응답) => {
 // })
 
 app.get('/list', async (요청, 응답) => {
+  //mongoDB의 이름이 'post' 임 
+  //그래서 db.collection('post')임
     let result = await db.collection('post').find().toArray()
     // 응답.send(result[1].content) 응답은 한번만
     응답.render('list.ejs', {글목록: result})
   })
 
-app.get('/time', (요청, 응답) => {
-  let nowtime = new Date()
-  응답.render('time.ejs', {시간: nowtime})
+app.get('/write', (요청, 응답) => {
+  응답.render('write.ejs')
+})
+
+app.post('/add', async(요청, 응답)=>{
+  await db.collection('post').insertOne({title: 요청.body.title, content: 요청.body.content})
+  // console.log(요청.body.title)
+  응답.redirect('/write')
 })
